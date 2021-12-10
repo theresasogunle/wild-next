8b7aa1fb1fc49ef12da07967edf4306520d82bf7
Oskar Rainer <rainer.oskar@gmail.com>
2021-12-10 12:32:40 +0100

    fix: remove console.logs

--- src/pages/api/ping.tsx
+++ src/pages/api/ping.tsx
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req, res);
   res.status(200).json({ ping: true });
 };
